4d056984c5612132567d1be0c01cb4f8b9ef822d
ipodobin <ipodobin@gmail.com>
2018-12-19 21:56:26 +0100

    closes #7

---
+++ pet-clinic-web/src/main/java/org/vsoft/vspetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package org.vsoft.vspetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
+    private String listVets() {
+
+        return "vets/index";
+    }
+
+}
