8104aa43b68d61ca3dfaa410aaa91fadd414053d
ipodobin <ipodobin@gmail.com>
2019-01-17 23:28:41 +0100

    added Speciality map Service closes #31

---
+++ pet-clinic-data/src/main/java/org/vsoft/vspetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package org.vsoft.vspetclinic.services;
+
+import org.vsoft.vspetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/vsoft/vspetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,11 @@
+package org.vsoft.vspetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import org.vsoft.vspetclinic.model.Speciality;
+import org.vsoft.vspetclinic.services.SpecialityService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+
+}
