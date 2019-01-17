231c61c6ec049dbc5354fd36df716d0b67f845ee
ipodobin <ipodobin@gmail.com>
2019-01-17 22:03:15 +0100

    added entity Visit closes #26

---
+++ pet-clinic-data/src/main/java/org/vsoft/vspetclinic/model/Visit.java
@@ -0,0 +1,35 @@
+package org.vsoft.vspetclinic.model;
+
+
+import java.time.LocalDate;
+
+public class Visit extends  BaseEntity {
+
+    private String description;
+    private LocalDate date;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
